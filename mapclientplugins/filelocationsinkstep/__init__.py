77a9be43e4f1286d16ecc42e19d0d74844330740
Hugh Sorby <h.sorby@auckland.ac.nz>
2021-11-01 10:19:35 +1300

    Set location to mapclient-plugins

--- mapclientplugins/filelocationsinkstep/__init__.py
+++ mapclientplugins/filelocationsinkstep/__init__.py
@@ -6,7 +6,7 @@ MAP Client Plugin
 __version__ = '0.2.0'
 __author__ = 'Richard Christie'
 __stepname__ = 'File Location Sink'
-__location__ = 'https://github.com/rchristie/mapclientplugins.filelocationsinkstep'
+__location__ = 'https://github.com/mapclient-plugins/mapclientplugins.filelocationsinkstep'
 
 # import class that derives itself from the step mountpoint.
 from mapclientplugins.filelocationsinkstep import step
